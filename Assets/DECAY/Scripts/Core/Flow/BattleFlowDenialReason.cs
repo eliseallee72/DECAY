2ef82c044f53e0df74ffc2e0ac9945e080992529
eliseallee72 <eliseallee72@gmail.com>
2026-08-16 16:45:03 -0500

    Step 5: remove superseded setup sub-turn flow denials

--- Assets/DECAY/Scripts/Core/Flow/BattleFlowDenialReason.cs
+++ Assets/DECAY/Scripts/Core/Flow/BattleFlowDenialReason.cs
@@ -5,8 +5,6 @@ namespace Decay
         None = 0,
         BattleAlreadyComplete = 1,
         WrongPhase = 2,
-        EnemySetupMustComplete = 3,
-        EnemySetupAlreadyComplete = 4,
         RollNotResolved = 5
     }
 }
